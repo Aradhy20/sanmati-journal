688ec1971d9d0d712c18a4cc976de58fba969fb6
Aradhy Jain <aradhyjain@Aradhys-MacBook-Air.local>
2026-05-31 13:12:21 +0530

    debug: force app.debug in bootstrap/app.php

--- sanmati-core/bootstrap/app.php
+++ sanmati-core/bootstrap/app.php
@@ -25,6 +25,9 @@ $app = Application::configure(basePath: dirname(__DIR__))
     })
     ->create();
 
+// Force debug mode temporarily to capture the production exception
+config(['app.debug' => true]);
+
 $app->usePublicPath(__DIR__.'/../../public_html');
 
 return $app;
